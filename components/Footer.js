50c6ba2bdb929c19a81c67ffd97d2560152da14a
Emmanuel Yee <emmanuel_yee_hong_wei@s2022.ssts.edu.sg>
2025-01-18 10:01:26 +0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,9 +3,17 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <>
-      <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
-      </footer>
+      <footer className="p-5 bg-gray-900 text-white text-center">
+    <p>&copy; 2025 Nextfolio</p>
+    <div className="flex justify-center space-x-4 mt-4">
+      <a href="#" className="hover:text-gray-400">X</a>
+      <a href="#" className="hover:text-gray-400">GitHub</a>
+      <a href="#" className="hover:text-gray-400">Instagram</a>
+      <a href="#" className="hover:text-gray-400">LinkedIn</a>
+      <a href="#" className="hover:text-gray-400">Email</a>
+      <a href="#" className="hover:text-gray-400">RSS</a>
+    </div>
+  </footer>
     </>
   )
 }
